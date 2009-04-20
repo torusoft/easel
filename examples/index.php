c677dee4343a059a9edc31e0a73811c98471185b
Tim Kelty <timkelty@gmail.com>
2009-04-20 16:46:31 -0400

    updating docs, grid examples

--- examples/index.php
+++ examples/index.php
@@ -3,4 +3,5 @@
   <h1 class="page-title">
     Welcome to Easel<sup class="small">®</sup>
   </h1>
+  <p class="text-center tagline">Reinventing the wheel is for <span class="turquoise">chumps</span>.</p>
 <?php include("includes/footer.php"); ?>
--- examples/pages/grid.php
+++ examples/pages/grid.php
@@ -11,21 +11,42 @@
   <div class="col-12 col-last">
     <h2><code>class="col-12 col-last"</code></h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    
   </div>
 </div>
 <div class="content">
-  <div class="prepend-12 col-12 col-last">
-    <h2><code>class="prepend-12 col-12 col-last"</code></h2>
+  <div class="prepend-4 col-20 col-last">
+    <h2><code>class="prepend-4 col-20 col-last"</code></h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
   </div>
 </div>
 <div class="content">
-  <div class="append-12 col-12 col-last">
+  <div class="append-16 col-8 col-last">
     <h2><code>class="append-12 col-12 col-last"</code></h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
   </div>
 </div>
-
-
+<div class="content">
+  <div class="col-12">
+    <h2><code>class="col-12"</code></h2>
+    <div class="col-6">
+      <h3><code>class="col-6"</code></h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div class="col-6 col-last">
+      <h3><code>class="col-6 col-last"</code></h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+  </div>
+  <div class="col-12 col-last">
+    <h2><code>class="col-12 col-last"</code></h2>
+    <div class="col-8">
+      <h3><code>class="col-8"</code></h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div class="col-4 col-last">
+      <h3><code>class="col-4 col-last"</code></h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+  </div>
+</div>
 <?php include("../includes/footer.php"); ?>
