cc7e0d619ddc135f2f55aa63622eba723486711d
Tim Kelty <timkelty@gmail.com>
2009-04-21 10:27:21 -0400

    rearranging dir structure

--- examples/includes/header.php
+++ examples/includes/header.php
@@ -5,27 +5,27 @@
   <title>Welcome to Easel</title>
   <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
   <link rel="apple-touch-icon" href="/assets/images/icons/apple-touch-icon.png">
-  <link href="/styles/reset.css" media="screen, projection" rel="stylesheet" type="text/css">
-  <link href="/styles/screen.css" media="screen, projection" rel="stylesheet" type="text/css">
-  <link href="/styles/forms.css" media="screen, projection" rel="stylesheet" type="text/css">
-  <link href="/styles/extras.css" media="screen, projection" rel="stylesheet" type="text/css">
-  <link href="/styles/admin.css" media="screen, projection" rel="stylesheet" type="text/css">
-  <link href="/styles/print.css" media="print" rel="stylesheet" type="text/css">
+  <link href="/framework/styles/reset.css" media="screen, projection" rel="stylesheet" type="text/css">
+  <link href="/framework/styles/screen.css" media="screen, projection" rel="stylesheet" type="text/css">
+  <link href="/framework/styles/forms.css" media="screen, projection" rel="stylesheet" type="text/css">
+  <link href="/framework/styles/extras.css" media="screen, projection" rel="stylesheet" type="text/css">
+  <link href="/framework/styles/admin.css" media="screen, projection" rel="stylesheet" type="text/css">
+  <link href="/framework/styles/print.css" media="print" rel="stylesheet" type="text/css">
   
   <!-- examples styles -->
   <link href="/examples/styles/screen.css" media="screen, projection" rel="stylesheet" type="text/css">
   
   <!--[if lt IE 8]>
-  <link rel="stylesheet" href="styles/ie.css" type="text/css" media="screen, projection">
+  <link rel="stylesheet" href="/framework/styles/ie.css" type="text/css" media="screen, projection">
   <![endif]-->
   
   <script type="text/javascript">if (document.documentElement) document.documentElement.className = 'js';</script>
-  <script src="/scripts/jquery-1.3.2.min.js" type="text/javascript"></script>
-  <script src="/scripts/jquery.form.js" type="text/javascript"></script>
-  <script src="/scripts/jquery.tinyvalidate.js" type="text/javascript"></script>
-  <script src="/scripts/jquery.tinyvalidate.rules.js" type="text/javascript"></script>
-  <script src="/scripts/extras.js" type="text/javascript"></script>
-  <script src="/scripts/default.js" type="text/javascript"></script>
+  <script src="/framework/scripts/jquery-1.3.2.min.js" type="text/javascript"></script>
+  <script src="/framework/scripts/jquery.form.js" type="text/javascript"></script>
+  <script src="/framework/scripts/jquery.tinyvalidate.js" type="text/javascript"></script>
+  <script src="/framework/scripts/jquery.tinyvalidate.rules.js" type="text/javascript"></script>
+  <script src="/framework/scripts/extras.js" type="text/javascript"></script>
+  <script src="/framework/scripts/default.js" type="text/javascript"></script>
 </head>
 <body>
 <?php include("nav.php"); ?>
